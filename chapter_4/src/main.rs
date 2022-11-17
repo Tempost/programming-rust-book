aa4de532b064ecd10a00d0cd2b46f7241447d0da
Cody Diamond <cody_diamond@tecnics.com>
2022-11-17 16:23:43 -0600

    Moves:85

--- chapter_4/src/main.rs
+++ chapter_4/src/main.rs
@@ -1,3 +1,6 @@
+// Box<T> is a pointer to a value of type T stored on the heap.
+// let box = Box::new(()) here box is just a point to the memory in heap land
+// the memory is dropped when leaving the scope aka {inside here}
 fn main() {
     println!("Hello, world!");
 }
