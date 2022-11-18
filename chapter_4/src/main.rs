50767214eede21e487901268fef224b0282a6a37
Cody <codyldiamond@gmail.com>
2022-11-17 21:20:34 -0600

    97

--- chapter_4/src/main.rs
+++ chapter_4/src/main.rs
@@ -3,4 +3,10 @@
 // the memory is dropped when leaving the scope aka {inside here}
 fn main() {
     println!("Hello, world!");
+    let s = vec!["udon".to_string(), "ramen".to_string(), "soba".to_string()];
+    // let t = s; // Value moved in this exmaple
+    // let u = s; here the compiler would throw, since s was moved into t already and is
+    // uninitalized. instead we can .clone() the value
+    let t = s.clone();
+    let u = s.clone();
 }
